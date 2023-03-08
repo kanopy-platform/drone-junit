af2f9c9a90c35754b806f9ed4aecbd07ef2bb787
David Katz <david.katz@mongodb.com>
2023-03-08 10:56:24 -0500

    gluing it togehter

--- cmd/drone-junit/main.go
+++ cmd/drone-junit/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/kanopy-platform/drone-junit/internal/card"
+	"github.com/kanopy-platform/drone-junit/internal/junit"
+)
+
 func main() {
+
+	suites, err := junit.Read("/tmp/unit-tests.xml") // TODO customize path with arg
+	if err != nil {
+		panic(err)
+	}
+
+	if err := card.WriteCard(suites); err != nil {
+		panic(err)
+	}
 
 }
