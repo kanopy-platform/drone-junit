9976a2cb17919bde2070fb938aa56e5de8cdcb1c
David Katz <david.katz@mongodb.com>
2023-03-08 10:56:14 -0500

    adding card package

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/kanopy-platform/drone-junit
 
 go 1.20
 
-require github.com/stretchr/testify v1.8.2
+require (
+	github.com/drone/drone-go v1.7.1
+	github.com/stretchr/testify v1.8.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
---
+++ internal/card/card.go
@@ -0,0 +1,40 @@
+package card
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/drone/drone-go/drone"
+	"github.com/kanopy-platform/drone-junit/internal/junit"
+)
+
+func WriteCard(suite *junit.Testsuites) error {
+
+	cardData, err := json.Marshal(suite)
+	if err != nil {
+		return err
+	}
+
+	card := &drone.CardInput{
+		Schema: "https://example.com/card.json",
+		Data:   cardData,
+	}
+
+	return write(card)
+}
+
+func write(card *drone.CardInput) error {
+	data, err := json.Marshal(card)
+	if err != nil {
+		return err
+	}
+	out := os.Stdout
+	encoded := base64.StdEncoding.EncodeToString(data)
+	io.WriteString(out, "\u001B]1338;")
+	io.WriteString(out, encoded)
+	io.WriteString(out, "\u001B]0m")
+	io.WriteString(out, "\n")
+	return nil
+}
